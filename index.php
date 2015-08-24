6b923c87594ef82a31e182d54348b2dd75003c4c
Chanala Wilangka <yaicw49@hotmail.com>
2015-08-24 19:47:30 +0700

    insert 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ob_start('ob_gzhandler');
+?>
+    <html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <?php //include("views/header.php"); ?>
+                <?php //include("views/content.php"); ?>
+                    <?php //include("views/footer.php"); ?>
+                        <script type="text/javascript" src="js/main.js"></script>
+        </div>
+    </body>
+
+    </html>
