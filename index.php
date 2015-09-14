6e8da88c82f0bc47e8be8fb895761282c143d159
Chanala Wilangka <yaicw49@hotmail.com>
2015-09-14 16:32:30 +0700

    update mata tag (viewport content)

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ob_start('ob_gzhandler');
     <head>
         <title></title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
         <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
         <link rel="stylesheet" href="css/style.css">
     </head>
