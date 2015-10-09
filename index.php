402896322dfad4c004e10188953e3b715eaf5fd2
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-10 01:28:00 +0700

    define html5

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 ob_start('ob_gzhandler');
 ?>
-    <html>
+    <!DOCTYPE html>
+    <html lang="en">
 
     <head>
         <title></title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
         <link rel="stylesheet" href="css/style.css">
     </head>
