21d21e96dacf6e155cd8c4e5dbefed0ab714ee18
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-26 18:48:47 +0700

    insert include.php

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 
     <head>
         <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
-        <link rel="stylesheet" href="css/style.css">
+        <?php include("views/include.php"); ?>
     </head>
 
     <body>
---
+++ views/include.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
+<link rel="stylesheet" href="css/style.css">
+<!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> -->
