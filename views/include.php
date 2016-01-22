39870875aa3be3c1a5e287c93e94802332e3bce6
Chanala Wilangka <yaicw49@hotmail.com>
2016-01-22 12:37:18 +0700

    reverse jquery.js

--- views/include.php
+++ views/include.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type="text/javascript" src="js/jquery.min.js?v=1001"></script>
+<script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
 <link rel="stylesheet" href="css/style.css">
 <!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> -->
