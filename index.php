0be359be1f7b79c93625b0ef0a19fd3570e80c22
Chanala Wilangka <c.wilangka@gmail.com>
2016-03-14 19:50:00 +0700

    change include

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
 
-    <!DOCTYPE html>
-    <html lang="en">
+<head>
+    <title></title>
+    <?php include("include.php"); ?>
+</head>
 
-    <head>
-        <title></title>
-        <?php include("include.php"); ?>
-    </head>
+<body>
+    <div id="wrapper">
+        <?php include("views/header.php"); ?>
+        <!-- Content ********************* -->
+        
 
-    <body>
-        <div id="wrapper">
-            <?php include("views/header.php"); ?>
-                <?php //include("views/content.php"); ?>
-                    <?php //include("views/footer.php"); ?>
-                        <script type="text/javascript" src="js/main.js"></script>
-        </div>
-    </body>
+        <!-- End ********************* -->
+        <?php include("views/footer.php"); ?>
+        <script type="text/javascript" src="js/main.js"></script>
+    </div>
+</body>
 
-    </html>
+</html>
