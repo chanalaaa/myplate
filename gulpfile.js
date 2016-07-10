606c0b54c75997b7e2d8ac97a6722d9b637959a6
Chanala Wilangka <c.wilangka@gmail.com>
2016-07-10 12:55:29 +0700

    Change template from php to Jade Template Engine

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,41 @@ var sass = require('gulp-ruby-sass');
 var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 1%', 'Firefox ESR', "ie 8", "ie 7"]
+    browsers: ['last 2 versions', '> 1%', 'Firefox ESR', "ie 8", "ie 7"]
 };
+var jade = require('gulp-jade');
+var gutil = require('gulp-util');
+var prettify = require('gulp-prettify');
+
 gulp.task('sass', function() {
-    return sass('scss/**/*.scss',{noCache: true})
+    return sass('scss/**/*.scss', { noCache: true })
         .on('error', sass.logError)
         .pipe(minifycss())
         .pipe(autoprefixer(autoprefixerOptions))
         .pipe(gulp.dest('css')).on('error', sass.logError);
 
+});
+
+gulp.task('jade', function() {
+    var YOUR_LOCALS = {};
+    var j = jade({});
+    j.on('error', function(e) {
+        gutil.log(e);
+        j.end();
+    });
+
+    return gulp.src('./views/*.jade')
+        .pipe(j)
+        .pipe(prettify({
+            indent_size: 2
+        }))
+        /*
+        .pipe(jade({
+            locals: YOUR_LOCALS,
+            pretty: true
+        }))
+        */
+        .pipe(gulp.dest('./'))
 });
 /*
 gulp.task('browser-sync', function() {
@@ -25,8 +51,9 @@ gulp.task('browser-sync', function() {
 */
 
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass', 'jade'], function() {
     gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("views/**/*.jade", ['jade']);
     //gulp.watch(['**/*.html'], browserSync.reload);
     //gulp.watch(['**/*.php'], browserSync.reload);
     //gulp.watch(['css/**/*.css'], browserSync.reload);
--- views/include.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type="text/javascript" src="js/vendors/jquery_v2.1.4.min.js?v=1001"></script>
-<link rel="stylesheet" href="css/style.css">
-
-<!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> -->
